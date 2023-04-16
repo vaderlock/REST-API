56350777ef6f928d1a5b7cfe60872aa325e310bf
vaderlock <smuntase@nyit.edu>
2023-04-16 16:50:09 -0400

    add authentication controller

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,6 +3,6 @@ import crypto from 'crypto';
 const SECRET = 'VADERLOCK-REST-API'; 
 
 export const random = () => crypto.randomBytes(128).toString('base64'); 
-export const authenticaiton = (salt: string, password: string) => {
+export const authentication = (salt: string, password: string) => {
     return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex')
 }
