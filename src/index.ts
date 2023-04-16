7ce77a36c7be9d6e1d09ffcbe4bf8a026240f116
vaderlock <smuntase@nyit.edu>
2023-04-16 15:41:11 -0400

    setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello TypeScript");
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors'; 
+
+const app = express(); 
+
+app.use(cors({
+    credentials: true,
+}))
+
+app.use(compression()); 
+app.use(cookieParser());
+app.use(bodyParser.json()); 
+
+const server = http.createServer(app); 
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000/');
+})
