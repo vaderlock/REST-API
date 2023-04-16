019623e50e6e2793451aed4e9c25300e37ebd96d
vaderlock <smuntase@nyit.edu>
2023-04-16 15:57:28 -0400

    mongodb setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors'; 
+import mongoose from 'mongoose'; 
 
 const app = express(); 
 
@@ -20,3 +21,9 @@ const server = http.createServer(app);
 server.listen(3000, () => {
     console.log('Server running on http://localhost:3000/');
 })
+
+const MONGO_URL  = 'mongodb+srv://vaderlock:nrTY8Fq4kUfHydVL@cluster0.v7rickp.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise; 
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
